6d5c9be381ef51e9f30897def51dfa3052caed6d
Valeria Torres <valeria.gongora@campusucc.edu.co>
2026-05-06 22:25:05 -0500

    Modelo Producto creado

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=20)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.IntegerField()
+    total = models.DecimalField(max_digits=12, decimal_places=2, editable=False)
+
+    def calcular_total(self):
+        return self.precio * self.cantidad
+
+    def save(self, *args, **kwargs):
+        self.total = self.calcular_total()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nombre
